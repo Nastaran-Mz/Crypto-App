0087c709be327c64d03d972de676915fdac8f495
nastaran-mz <mazaherinastaran@gmail.com>
2024-09-24 10:59:11 +0330

    data fetch from api

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -1,11 +1,12 @@
 
 
-export function Tablecoin() {
-    
+function TableCoin({ coins }) {
+    console.log(coins);
 
     return (
         <>
-            
+            <h1>table</h1>
         </>
     )
 }
+export default TableCoin;
--- src/components/templats/HomePage.jsx
+++ src/components/templats/HomePage.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { TableCoin } from "../modules/Tablecoin";
 
 
 export function Homepage() {
-    
-
-    return (
-        <>
-           <div>HomePage</div> 
-        </>
+  const [coins, Setcoins] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per-page=20&page=1&sparkline=false&local=en&x_cg_demo_api_key=CG-53sb6M8EEhHpTNaCnM3ncsKY"
     )
+      .then((res) => res.json())
+      .then((json) => Setcoins(json));
+  }, []);
+
+  return (
+    <>
+      <div>
+         <TableCoin coins={coins}/>
+      </div>
+    </>
+  );
 }
