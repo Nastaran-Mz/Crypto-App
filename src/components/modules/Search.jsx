37a32f272344e1b1de96fb2a5b02e2fb08ba6314
nastaran-mz <mazaherinastaran@gmail.com>
2024-10-01 17:45:51 +0330

    show loading in search box

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -5,30 +5,37 @@ import { RotatingLines } from "react-loader-spinner";
 // eslint-disable-next-line react/prop-types
 export function Search({ currency, setCurrency }) {
   const [text, setText] = useState("");
-  const [coins,setCoins] = useState([]);
-  const [isLoading,setIsLoading] = useState(true);
-  useEffect (() => {
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
     const controller = new AbortController();
     setCoins([]);
-    if (!text) return;
+    if (!text) {setIsLoading(false);
+        return;
+    }
 
     const search = async () => {
-        try{
-            const res = await fetch(searchCoin(text),{signal:controller.signal});
-            const json = await res.json();
-            if (json.coins) {setCoins(json.coins)} else{
-                alert(json.status.error_message)
-            };
-        }catch (error){
-            if (error.name ==! "AbortError"){
-                alert(error.message);
-            }
+      try {
+        const res = await fetch(searchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        if (json.coins) {
+          setCoins(json.coins);
+          setIsLoading(false);
+        } else {
+          alert(json.status.error_message);
         }
-      
+      } catch (error) {
+        if (error.name == !"AbortError") {
+          alert(error.message);
+        }
+      }
     };
+    setIsLoading(true);
     search();
     return () => controller.abort();
-  },[text])
+  }, [text]);
 
   return (
     <>
@@ -45,13 +52,22 @@ export function Search({ currency, setCurrency }) {
           <option value="jpy">JPY</option>
         </select>
         <div>
-            {isLoading && <RotatingLines width="50px" height="50px"/>}
-            <ul>
-                {coins.map(coin => <li key={coin.id}>
-                    <img src={coin.thumb} alt={coin.name}/>
-                    <p>{coin.name}</p>
-                </li>)}
-            </ul>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeWidth="2"
+              strokeColor="#3874ff"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
