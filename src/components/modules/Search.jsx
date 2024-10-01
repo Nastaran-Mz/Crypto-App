86bfa1bfa1bfdd3d2edb3afa30a64411b81f8e09
nastaran-mz <mazaherinastaran@gmail.com>
2024-10-01 16:52:14 +0330

    add search functionality to the project

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import { searchCoin } from "../../services/cryptoApi";
+
 export function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins,setCoins] = useState([]);
+
+  useEffect (() => {
+    if (!text) return;
+
+    const search = async () => {
+         const res = await fetch (searchCoin(text));
+         const json = await res.json();
+         if (json.coins) setCoins(json.coins);
+    }
+    search();
+  },[text])
+
   return (
     <>
       <div>
-        <input type="text" />
-        <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={text}
+          onChange={(e) => setCurrency(e.target.value)}
+        />
+        <select value={currency} onChange={(e) => setText(e.target.value)}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="jpy">JPY</option>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,8 @@
+const BASE_URL = "https://api.coingecko.com/api/v3";
+const API_LEY = "CG-53sb6M8EEhHpTNaCnM3ncsKY";
+const getCoinList = (page, currency) =>
+  `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&sparkline=false&local=en&x_cg_demo_api_key=${API_LEY}`;
 
-const BASE_URL = "https://api.coingecko.com/api/v3"
-const API_LEY = "CG-53sb6M8EEhHpTNaCnM3ncsKY"
-const getCoinList = (page , currency) => {
-   return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&sparkline=false&local=en&x_cg_demo_api_key=${API_LEY}`
-};
-export {getCoinList};
+const searchCoin = (query) =>
+  `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_LEY}`;
+export { getCoinList , searchCoin };
